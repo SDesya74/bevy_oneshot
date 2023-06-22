778ee503c88c45510b6caf3d9b15dda29a61398e
SDesya74 <sdesya74@gmail.com>
2023-06-22 14:12:17 +0300

    feat: docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,21 @@
+#![doc = include_str!("../README.md")]
+
 use bevy::prelude::*;
 
 /// A trait for oheshot systems
 pub trait CommandRunOnce<'w, 's> {
     /// Run system once
     ///
-    /// For version with In arguments look at run_once_with
+    /// For version with [In][`bevy::prelude::In`] arguments look at [`CommandRunOnce::run_once_with`].
     fn run_once<S, Marker: 'static>(&mut self, command: S)
     where
         S: SystemParamFunction<Marker, In = ()>;
 
+    /// Run system once with arguments
+    ///
+    /// For version without arguments look at [`CommandRunOnce::run_once`].
+    ///
+    /// Note: [In][`bevy::prelude::In`] argument should be **first**.
     fn run_once_with<S, Marker: 'static, Args: Send + 'static>(&mut self, command: S, args: Args)
     where
         S: SystemParamFunction<Marker, In = Args>;
